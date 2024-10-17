2f28f07a32925b4b082cd8f53c8db490b4dec6d9
Kartik-Hiremath <kartikhiremath001@gmail.com>
2024-10-17 22:10:08 +0530

    Added library for getting ETH to USD and to convert given ETH to USD.

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -3,36 +3,20 @@
 
 pragma solidity ^0.8.20;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe{
+
+    using PriceConverter for uint256;
 
     uint256 minUsd = 5 * 1e18;
     address[] public funders;
     mapping(address funder => uint256 amount) funderToAmountMap;
 
     function fund() public payable{
-        require(convertion(msg.value) >= minUsd, "Ether amount not sufficient.Please send atleast $5 worth of ether.");
+        require(msg.value.getConversionRate() >= minUsd, "Ether amount not sufficient.Please send atleast $5 worth of ether.");
         funders.push(msg.sender);
         funderToAmountMap[msg.sender] = funderToAmountMap[msg.sender] + msg.value;
     }
 
-    function getPrice() public view returns(uint256) {
-        // Address (ETH to USD) : 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        // ABI : AggregatorV3Interface gives the necessary functions.
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (,int256 price, , , ) = priceFeed.latestRoundData();
-        return uint256(price * 1e10);
-    }
-
-    function convertion(uint256 _amount) public view returns(uint256) {
-        uint256 ethPrice = getPrice();
-        uint256 ethToUsd = (_amount * ethPrice) / 1e18;
-        return ethToUsd;
-    }
-
-    function getVersion() public view returns(uint256, uint8) {
-        AggregatorV3Interface obj = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        return (obj.version(), obj.decimals());
-    }
 }
---
+++ contracts/PriceConverter.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter{
+
+    function getPrice() public view returns(uint256) {
+        // Address (ETH to USD) : 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        // ABI : AggregatorV3Interface gives the necessary functions.
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (,int256 price, , , ) = priceFeed.latestRoundData();
+        return uint256(price * 1e10);
+    }
+
+    function getConversionRate(uint256 _amount) public view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethToUsd = (_amount * ethPrice) / 1e18;
+        return ethToUsd;
+    }
+
+    function getVersionNDecimals() public view returns(uint256, uint8) {
+        AggregatorV3Interface obj = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return (obj.version(), obj.decimals());
+    }
+}

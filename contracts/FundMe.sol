f084c39b77b59e80df7d032e9e5a7d5b1123a899
Kartik-Hiremath <kartikhiremath001@gmail.com>
2024-10-19 12:38:16 +0530

    Added withdrawal function.

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -9,14 +9,24 @@ contract FundMe{
 
     using PriceConverter for uint256;
 
-    uint256 minUsd = 5 * 1e18;
+    uint256 minUsd = 2 * 1e18;
     address[] public funders;
     mapping(address funder => uint256 amount) funderToAmountMap;
 
     function fund() public payable{
-        require(msg.value.getConversionRate() >= minUsd, "Ether amount not sufficient.Please send atleast $5 worth of ether.");
+        require(msg.value.getConversionRate() >= minUsd, "Ether amount not sufficient.Please send atleast $2 worth of ether.");
         funders.push(msg.sender);
         funderToAmountMap[msg.sender] = funderToAmountMap[msg.sender] + msg.value;
     }
 
+    function withdraw() public {
+        for(uint256 i=0; i<funders.length; i++){
+            funderToAmountMap[funders[i]] = 0;
+        }
+        funders = new address[](0); // address array of length 0.
+        // payable(msg.sender).transfer(address(this).balance);
+        // bool transactionSuccess = payable(msg.sender).send(address(this).balance);
+        (bool transactionSuccess, ) = payable(msg.sender).call{value : address(this).balance}("");
+        require(transactionSuccess, "The Withdrawal Failed!");
+    }
 }

80db5893ddbd41c3f71c686da35fa53052b265e7
Kartik-Hiremath <kartikhiremath001@gmail.com>
2024-10-19 17:43:48 +0530

    Reduced gas by using constant and immutable.

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,33 +1,34 @@
 // SPDX-License-Identifier: MIT
-// TODO : 1. Set a min value and get the funds. 2. Withdraw.
+// TODO : 1. Set a min value and get the funds. 2. Withdraw. 3. Optimize.
 
 pragma solidity ^0.8.20;
 
 import {PriceConverter} from "./PriceConverter.sol";
 
+error NotOwner();
+
 contract FundMe{
 
     using PriceConverter for uint256;
 
-    uint256 public minUsd = 2 * 1e18;
+    uint256 public constant MIN_USD = 2 * 1e18;
     address[] public funders;
     mapping(address funder => uint256 amount) public funderToAmountMap;
-    address public ownerOfContract;
+    address public immutable i_OwnerOfContract;
 
-    constructor(){  // No need to mention public.
-        ownerOfContract = msg.sender;
+    constructor(){
+        i_OwnerOfContract = msg.sender;
     }
 
     function fund() public payable{
-        require(msg.value.getConversionRate() >= minUsd, "Ether amount not sufficient.Please send atleast $2 worth of ether.");
+        require(msg.value.getConversionRate() >= MIN_USD, "Ether amount not sufficient.Please send atleast $2 worth of ether.");    //msg.value is the first parameter of the function getConversionRate.
         funders.push(msg.sender);
         funderToAmountMap[msg.sender] = funderToAmountMap[msg.sender] + msg.value;
     }
 
     function withdraw() public ownerOnly {
-        for(uint256 i=0; i<funders.length; i++){
+        for(uint256 i=0; i<funders.length; i++)
             funderToAmountMap[funders[i]] = 0;
-        }
         funders = new address[](0); // address array of length 0.
         // payable(msg.sender).transfer(address(this).balance);
         // bool transactionSuccess = payable(msg.sender).send(address(this).balance);
@@ -36,7 +37,16 @@ contract FundMe{
     }
 
     modifier ownerOnly() {
-        require(msg.sender == ownerOfContract, "You are not the owner of this contract.");
+        // require(msg.sender == i_OwnerOfContract, "You are not the owner of this contract.");
+        // require(msg.sender == i_OwnerOfContract, NotOwner());
+        if(msg.sender != i_OwnerOfContract)
+            revert NotOwner();
         _;  // This means continue the rest of the code.
     }
 }
+
+/* NOTE :
+    1. Initially the contract costs : 595855 gas, after using constant: 575550 gas, after using immutable: 553047
+    2. MIN_USD call : 2400 gas to 300 gas after using constant.
+    3. For the constructor no need to specify public.
+*/
